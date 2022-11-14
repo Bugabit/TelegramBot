06974dca0a7a180e6b2f35bd5c105120d71ce9b3
Javi <javisr997@gmail.com>
2022-11-14 15:38:25 +0100

    Creada funcion para controlar el envio POST del JSON desde GitHub

--- src/main/java/com/example/telegrambot/BotController.java
+++ src/main/java/com/example/telegrambot/BotController.java
@@ -1,46 +1,45 @@
 package com.example.telegrambot;
 
-import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.request.ForceReply;
-import com.pengrad.telegrambot.model.request.ParseMode;
-import com.pengrad.telegrambot.request.SendMessage;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/")
 public class BotController {
 
-    static final String BOT_TOKEN = "5798304539:AAFgpy_ost1nI7yR7BCTnlhx9cdfkGU8qxU";
-    static final long CHAT_ID = 1636051587L;
-    static final long GROUP_ID = -1001561970415L;
-    static final long TEST_GROUP_ID = -860576555L;
+    private final BotService bot;
+
+    @Autowired
+    public BotController(BotService bot) {
+        this.bot = bot;
+    }
 
     @GetMapping
     public String index() {
-        return "<p><a href=\"/update\">Enviar mensaje</a></p>";
+        return  "<p><a href=\"/update\">Enviar mensaje</a></p>\n" +
+                "<form action=\"/winner\" method=\"POST\">\n" +
+                "  <label for=\"nombre\">Nombre: <input type=\"text\" name=\"nombre\" id=\"nombre\"></label>\n" +
+                "  <label for=\"apellidos\">Apellidos: <input type=\"text\" name=\"apellidos\" id=\"apellidos\"></label>\n" +
+                "  <input type=\"submit\" value=\"Enviar\">\n" +
+                "</form>";
     }
 
-    @GetMapping("update")
-    public RedirectView sendMessage() {
-        TelegramBot bot = new TelegramBot(BOT_TOKEN);
-
+    @GetMapping("test")
+    public RedirectView testMessage() {
         // TODO: Get JSON and Parse to get winner(s)
 
-        // Creating response message
-        SendMessage request = new SendMessage(TEST_GROUP_ID, "Mensaje de prueba desde Spring")
-                .parseMode(ParseMode.HTML)
-                .disableWebPagePreview(true)
-                .disableNotification(true)
-                .replyMarkup(new ForceReply());
-
-        // Executing request (sending message)
-        bot.execute(request);
+        // Sending message
+        bot.sendMessage("Heyyy muy buenas a todos");
 
         // Redirect to index
         return new RedirectView("");
     }
 
+    @PostMapping("winner")
+    public String sendWinner(@RequestBody String body) {
+        return body;
+    }
 }
---
+++ src/main/java/com/example/telegrambot/BotService.java
@@ -0,0 +1,38 @@
+package com.example.telegrambot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.request.ForceReply;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BotService {
+
+    static final String BOT_TOKEN = "5798304539:AAFgpy_ost1nI7yR7BCTnlhx9cdfkGU8qxU";
+    static final long CHAT_ID = 1636051587L;
+    static final long GROUP_ID = -1001561970415L;
+    static final long TEST_GROUP_ID = -860576555L;
+
+    private final TelegramBot bot;
+
+    public BotService() {
+        this.bot = new TelegramBot(BOT_TOKEN);
+    }
+
+    public TelegramBot getBot() {
+        return bot;
+    }
+
+    public void sendMessage(String message) {
+        // Creating response message
+        SendMessage request = new SendMessage(TEST_GROUP_ID, message)
+                .parseMode(ParseMode.HTML)
+                .disableWebPagePreview(true)
+                .disableNotification(true)
+                .replyMarkup(new ForceReply());
+
+        // Executing request (sending message)
+        bot.execute(request);
+    }
+}
